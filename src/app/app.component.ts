82ffd30d8164ce756f827a0c6b687a1822532145
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2025-01-28 13:04:20 -0600

    operador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 //Esto es propio de rxjs y crea numeros cada cierto tiempo
-import { interval } from 'rxjs';
+import { interval, map } from 'rxjs';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,13 +10,17 @@ export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
   ngOnInit(): void {
     //El tiempo en milisegundos y la constante es para que se limpie del dom al construirse el elemento
-    const subscription = interval(1000).subscribe({
-      next: (value) => console.log(value),
-      //Se llama si el observable ya no emite nada
-      complete: () => {},
-      //Por si ocurre un error
-      error: () => console.log('Ops..'),
-    });
+    const subscription = interval(1000)
+      //Esto es un operador
+      .pipe(map((val) => val * 2))
+      .subscribe({
+        next: (value) => console.log(value),
+        //Se llama si el observable ya no emite nada
+        complete: () => {},
+        //Por si ocurre un error
+        error: () => console.log('Ops..'),
+      });
+    //Esto destruye la suscripción
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
     });
