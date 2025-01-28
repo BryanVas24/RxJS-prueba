c9b05fa19c45fe5522304310f3f86ec8eaaa36e7
Bryan Vásquez <159486619+BryanVas24@users.noreply.github.com>
2025-01-28 12:56:25 -0600

    Creando y usando un Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+//Esto es propio de rxjs y crea numeros cada cierto tiempo
+import { interval } from 'rxjs';
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+  ngOnInit(): void {
+    //El tiempo en milisegundos y la constante es para que se limpie del dom al construirse el elemento
+    const subscription = interval(1000).subscribe({
+      next: (value) => console.log(value),
+      //Se llama si el observable ya no emite nada
+      complete: () => {},
+      //Por si ocurre un error
+      error: () => console.log('Ops..'),
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
